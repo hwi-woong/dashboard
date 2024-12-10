c4a92f8a01f20f38f09b6f81f25e9f3ed793c258
hwi-wwong <mattnaver@gmail.com>
2024-12-10 17:06:30 +0900

    Create Chart.js

---
+++ src/components/Chart.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import { Chart, registerables } from 'chart.js';
+import './GraphSection.css';
+
+// Chart.js의 구성 요소를 등록
+Chart.register(...registerables);
+
+const GraphSection = ({ title, data }) => {
+  return (
+    <div className="graph-section">
+      <h3>{title}</h3>
+      <Line data={data} options={{ responsive: true }} />
+    </div>
+  );
+};
+
+export default GraphSection;

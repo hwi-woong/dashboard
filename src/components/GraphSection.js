fafe217ed20f7ba2830fd437a3710d5d3a5a35f2
hwi-wwong <mattnaver@gmail.com>
2024-12-12 09:49:39 +0900

    Update GraphSection.js

--- src/components/GraphSection.js
+++ src/components/GraphSection.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { Line } from 'react-chartjs-2';
-import { Chart, registerables } from 'chart.js';
-import './GraphSection.css';
+import { Line } from 'react-chartjs-2'; // Line 컴포넌트를 가져옵니다.
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 
-// Chart.js의 구성 요소를 등록
-Chart.register(...registerables);
+// Chart.js에서 필요한 구성 요소 등록
+Chart.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 const GraphSection = ({ title, data }) => {
   return (
     <div className="graph-section">
       <h3>{title}</h3>
-      <Line data={data} options={{ responsive: true }} />
+      {/* Line 컴포넌트에 데이터와 옵션 전달 */}
+      <Line data={data} options={{ responsive: true, maintainAspectRatio: false }} />
     </div>
   );
 };
